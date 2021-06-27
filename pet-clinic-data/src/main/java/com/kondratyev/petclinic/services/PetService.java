8dbd6cedd563579808f4c270a1ff95e6ad3f91a4
kondratyev-a <alex.a.kondratyev@gmail.com>
2021-06-27 14:55:14 +0700

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/kondratyev/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.kondratyev.petclinic.services;
+
+import com.kondratyev.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
