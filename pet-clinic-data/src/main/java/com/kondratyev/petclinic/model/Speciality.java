1ac7d73da332f982500663e406c2e7ca75c052ea
kondratyev-a <alex.a.kondratyev@gmail.com>
2021-07-14 15:15:41 +0700

    Create Vet Speciality Entity, associate to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/com/kondratyev/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.kondratyev.petclinic.model;
+
+public class Speciality extends BaseEntity{
+
+    private String descriprion;
+
+    public String getDescriprion() {
+        return descriprion;
+    }
+
+    public void setDescriprion(String descriprion) {
+        this.descriprion = descriprion;
+    }
+}
