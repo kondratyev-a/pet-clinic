5ff2ecd78389db300250543a099845c191ee1bfb
kondratyev-a <alex.a.kondratyev@gmail.com>
2021-07-21 15:26:45 +0700

    Load Visit on Startup in Bootstrap. Closes #48

--- pet-clinic-web/src/main/java/com/kondratyev/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/kondratyev/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.kondratyev.petclinic.bootstrap;
 
 import com.kondratyev.petclinic.model.*;
-import com.kondratyev.petclinic.services.OwnerService;
-import com.kondratyev.petclinic.services.PetTypeService;
-import com.kondratyev.petclinic.services.SpecialtyService;
-import com.kondratyev.petclinic.services.VetService;
+import com.kondratyev.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialtyService specialtyService) {
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -86,6 +85,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescriprion("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
